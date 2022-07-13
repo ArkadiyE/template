705cf8caf99d0c08306345366bd51a1535ab853b
ArkadiyE <106632787+ArkadiyE@users.noreply.github.com>
2022-07-13 10:42:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,32 @@
 from setuptools import setup, find_packages
+setup(
+   name="template",
+   version="0.1.0",
+   author="Arkadiy Epshtein",
+   author_email="arkadiy.epshtein@stonybrook.edu",
+   packages=find_packages(),
+   scripts=["bin/CommandLine1"],
+   url="http://pypi.python.org/pypi/template/",
+   license= "LISENCE.txt",
+   description='A program that creates notebook templates.',
+   long_description=open('README.txt').read(),
+   install_requires=[
+       "Django >= 1.1.1",
+       "pytest",
+       "Jupyter",
+       "bash"
+   ],
+    classifiers=['Intended Audience :: Science/Research',
+                 'Programming Language :: Python',
+                 'Topic :: Scientific/Engineering :: Bio-Informatics',
+                 'Topic :: Scientific/Engineering :: Visualization',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5'],
+    zip_safe=False)
