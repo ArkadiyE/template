68ea89f1ee424bca8772f0af9783fb4ca2d9e0ab
ArkadiyE <106632787+ArkadiyE@users.noreply.github.com>
2022-07-19 09:19:00 -0700

    Update and rename make_directories.py to Make_Directories.py

---
+++ template/Make_Directories.py
@@ -0,0 +1,21 @@
+def make_directories(base_dir):
+  base_dir = Path(base_dir)
+    base_dir.mkdir()
+    ref = Path('ref')
+    ref = Path.cwd() / base_dir / ref
+    ref.mkdir()
+    data = Path('data')
+    data = Path.cwd() / base_dir / data
+    data.mkdir()
+    notebook = Path('notebook')
+    notebook = Path.cwd() / base_dir / notebook
+    notebook.mkdir()
+    notebook.exists()
+    results = Path('results')
+    results = Path.cwd() / base_dir / results
+    results.mkdir()
+    results.exists()
+    utils = Path('utils')
+    utils = Path.cwd() / base_dir / utils
+    utils.mkdir()
+    utils.exists()
