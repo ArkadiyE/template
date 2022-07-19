2a17f1df544b0f5a8b008f92e156233d58b71836
ArkadiyE <106632787+ArkadiyE@users.noreply.github.com>
2022-07-19 09:32:57 -0700

    Update CommandLine1.py

--- template/bin/CommandLine1.py
+++ template/bin/CommandLine1.py
@@ -6,8 +6,9 @@ from pathlib import PosixPath
 from pathlib import PurePosixPath
 import os
 import argparse
-#import Create_Symlink.py
-#import Do_Render.py
+from template.Make_Directories import make_directories
+from template.Creat_Symlink import make_symlink
+from template.Do_Render import do_render
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
 
@@ -20,35 +21,12 @@ def main():
     parser.add_argument("-syl", "--symlink", action="store_true")
     parser.add_argument("-t", "--template", action="store_true")
     args = parser.parse_args()
-    args.base_dir = Path(args.base_dir)
-    args.base_dir.mkdir()
-    ref = Path('ref')
-    ref = Path.cwd() / args.base_dir / ref
-    ref.mkdir()
-    data = Path('data')
-    data = Path.cwd() / args.base_dir / data
-    data.mkdir()
-    notebook = Path('notebook')
-    notebook = Path.cwd() / args.base_dir / notebook
-    notebook.mkdir()
-    notebook.exists()
-    results = Path('results')
-    results = Path.cwd() / args.base_dir / results
-    results.mkdir()
-    results.exists()
-    utils = Path('utils')
-    utils = Path.cwd() / args.base_dir / utils
-    utils.mkdir()
-    utils.exists()
+    make_directories(args.base_dir)
     if args.symlink:
         parser.add_argument("link", type=str)
         parser.add_argument("destination", type=str)
         args = parser.parse_args()
-        def make_symlink():
-            reflink = args.link + 'ref'
-            reflink = ref / Path(args.link)
-            dest = '/home/arkadiy/reference/' + args.destination
-            reflink.symlink_to(Path(dest))
+        make_symlink(args.link, args.destination)
     if args.template:
         parser.add_argument("filename", type=str)
         parser.add_argument("PATTERN", type=str)
@@ -58,13 +36,8 @@ def main():
         args = parser.parse_args()
         kwargs = vars(args)
         kwargs = args.PATTERN, args.RUNID, args.NPROC, args.base_dir
-        def do_render():
-            env = Environment(loader=FileSystemLoader("."),autoescape=select_autoescape())
-            path = Path(args.filename)
-            path.exists()
-            template = env.get_template(args.filename)
-            return template.render(**kwargs)
-            
+        do_render(args.filename, **kwargs):
+          
 if __name__ == "__main__":
     main()
 
