49d7eaa4a95a9dd22bcb05d46e368f901f1e6189
ArkadiyE <106632787+ArkadiyE@users.noreply.github.com>
2022-07-20 08:31:26 -0700

    Create do_render.py

---
+++ template/functions/do_render.py
@@ -0,0 +1,11 @@
+
+def do_render(filename, **kwargs):
+            env = Environment(loader=FileSystemLoader("."),autoescape=select_autoescape())
+            path = Path(filename)
+            path.exists()
+            template = env.get_template(filename)
+            return template.render(**kwargs)
+            stat = template.render(**kwargs)
+            rendered_template = Path('rendered_template')
+            rendered_template = notebook / stat.ipynb
+            rendered_template.mkdir()
