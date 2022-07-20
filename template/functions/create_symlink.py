adb53ea0dab76b4197da800f0fb2b94fbf2436a5
ArkadiyE <106632787+ArkadiyE@users.noreply.github.com>
2022-07-20 08:31:06 -0700

    Create create_symlink.py

---
+++ template/functions/create_symlink.py
@@ -0,0 +1,5 @@
+def make_symlink(link, dest):
+            reflink = link + 'ref'
+            reflink = ref / Path(link)
+            destination = '/home/arkadiy/reference/' + dest
+            reflink.symlink_to(Path(destination))
