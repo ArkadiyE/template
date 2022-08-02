11f21f9e2b40567be90361f4fd2e5018dc2b0e0d
arkadiy <arkadiy.epshtein@stonybrook.edu>
2022-08-02 12:25:53 -0400

    removed unnecessary files

--- template/create_symlink.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-def make_symlink(link, dest):
-            reflink = link + 'ref'
-            reflink = ref / Path(link)
-            destination = '/home/arkadiy/reference/' + dest
-            reflink.symlink_to(Path(destination))
