88eb52bcc9d17d4d70477cb16d52989bc2bfd927
arkadiy <arkadiy.epshtein@stonybrook.edu>
2022-08-03 13:00:20 -0400

    changed the main method

--- template/bin/CommandLine1.py
+++ template/bin/CommandLine1.py
@@ -22,7 +22,7 @@ def main():
     make_directories(args.set_dir)
 
     if args.symlink:
-        
+        parser = argparse.ArgumentParser()
         parser.add_argument("link", type=str)
         parser.add_argument("destination", type=str)
         #args = parser.parse_args()
@@ -30,7 +30,7 @@ def main():
         make_symlink(args.link, args.destination)
         
     if args.template:
-        
+        parser = argparse.ArgumentParser()
         parser.add_argument("filename", type=str)
         parser.add_argument("PATTERN", type=str)
         parser.add_argument("RUNID", type=str, default="")
