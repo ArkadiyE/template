8142704c5702df75e34b289b6bf337e722565c16
arkadiy <arkadiy.epshtein@stonybrook.edu>
2022-08-05 12:45:49 -0400

    changed the if statement in create_symlink

--- template/functions/create_symlink.py
+++ template/functions/create_symlink.py
@@ -3,6 +3,6 @@ from pathlib import Path
 def make_symlink(link, set_dir, dest):
             
             reflink = Path(set_dir) / Path(link)
-            if not dest.exists():
+            if not Path(dest).exists():
                 raise ValueError(f'{dest} does not exist')
             reflink.symlink_to(Path(dest))
