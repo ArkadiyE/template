ba723c0dca9528c97bedae4cc7c4478631d55b80
arkadiy <arkadiy.epshtein@stonybrook.edu>
2022-08-05 12:41:01 -0400

    changed the parameters that create_symlink takes

--- template/bin/CommandLine1.py
+++ template/bin/CommandLine1.py
@@ -13,7 +13,6 @@ def main():
     parser.add_argument("-syl", "--symlink", action="store_true")
     parser.add_argument("-t", "--template", action="store_true")
     parser.add_argument("-lnk", "--link", type=str)
-    parser.add_argument("-rflnk", "--reference_link", type=str)
     parser.add_argument("-dst", "--destination", type=str) 
     parser.add_argument("-fn", "--filename", type=str)
     parser.add_argument("-PT", "--PATTERN", type=str)
@@ -28,7 +27,7 @@ def main():
 
     if args.symlink:
         print(args)
-        make_symlink(args.link, args.reference_link, args.destination)
+        make_symlink(args.link, args.set_dir, args.destination)
         
     if args.template:
         
--- template/functions/create_symlink.py
+++ template/functions/create_symlink.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 
-def make_symlink(link, reflink, dest):
+def make_symlink(link, set_dir, dest):
             
-            reflink = ref / Path(link)
-            destination = '/home/arkadiy/reference/' + dest
-            print(destination.exists())
-            reflink.symlink_to(Path(destination))
+            reflink = Path(set_dir) / Path(link)
+            if not dest.exists():
+                raise ValueError(f'{dest} does not exist')
+            reflink.symlink_to(Path(dest))
