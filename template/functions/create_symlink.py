8db6e2100cf9f296bdc589128ac65c9bf618c9a3
arkadiy <arkadiy.epshtein@stonybrook.edu>
2022-08-05 12:16:12 -0400

    added a print statement that displays whether or not destination exists

--- template/functions/create_symlink.py
+++ template/functions/create_symlink.py
@@ -4,4 +4,5 @@ def make_symlink(link, dest):
             reflink = link + 'ref'
             reflink = ref / Path(link)
             destination = '/home/arkadiy/reference/' + dest
+            print(destination.exists())
             reflink.symlink_to(Path(destination))
